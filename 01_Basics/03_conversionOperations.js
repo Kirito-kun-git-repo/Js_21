59051ad26356996a7ac86b83c97e39c2f1dff916
Kirito-kun-git-repo <kiritokunonline21@gmail.com>
2024-09-17 06:03:15 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,24 @@
+let score ="true"
+// console.log(typeof score);
+// console.log(typeof (score));
+let valueInNumber =Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+//NaN-Not a number
+//score=null o/p 0
+//undefined o/p NaN
+//true o/p 1
+let isLoggedIn="Chaman";
+let booleanIsLoggedIn =Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+// 1=> true,0=>false, ""=>false ,"chaman"=>true
+let someNumber=33;
+let stringNumber =String(someNumber);
+console.log( stringNumber);
+
+
+
+
+
+
+
